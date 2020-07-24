6d71e2a3086ea65a9a401ec2ad0710b837a72d84
hiroyuki takai <41976285+htakai@users.noreply.github.com>
2020-07-24 09:51:11 +0900

    Add files via upload

---
+++ news3/request/IndexRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * news aplication ホームページ表示に係わるリクエスト操作クラス
+ *
+ * @author Hiroyuki Takai <watashitakai@gmail.com>
+ * @since 2018/07/
+ */
+
+
+namespace news3\request;
+
+use framework3\mvc\Request;
+use framework3\F_util;
+use framework3\mvc\BadRequestException;
+
+class IndexRequest extends Request
+{
+}
